c0cc31b805f6c9da802b21e229d5438e80c4cfc7
Elnara <kimelim85@gmail.com>
2022-12-22 15:00:01 +0600

    Chapter 4 fully done

---
+++ cmd/api/movies.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"awesomeProject/internal/data"
+	"awesomeProject/internal/validator"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+// the interpolated "id" parameter from the current URL and include it in a placeholder
+// response.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// We can then use the ByName() method to get the value of the "id" parameter from
+	// the slice. In our project all movies will have a unique positive integer ID, but
+	// the value returned by ByName() is always a string. So we try to convert it to a
+	// base 10 integer (with a bit size of 64). If the parameter couldn't be converted,
+	// or is less than 1, we know the ID is invalid, so we use the http.NotFound()
+	// function to return a 404 Not Found response.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module awesomeProject
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
