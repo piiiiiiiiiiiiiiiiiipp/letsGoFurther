75207799ec9e046537d5fcec136a833037687822
Elnara <kimelim85@gmail.com>
2023-02-23 20:39:30 +0600

    some changes in migrations

--- migrations/000003_change_version_type.up.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-ALTER TABLE movies ALTER COLUMN version DROP DEFAULT,
-ALTER COLUMN version TYPE uuid USING (uuid_generate_v4()),
-    ALTER COLUMN version SET DEFAULT uuid_generate_v4()
